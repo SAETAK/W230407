f9552c3a9d935927d4728bf9367e62304600386f
SAETAK <ihjang404@gmail.com>
2023-04-07 10:23:32 +0900

    Add Lotto System

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+	int Ball[45] = { 0 };
+
+	srand((unsigned int)time(0));
+	
+	for (int i = 0; i < 45; ++i)
+	{
+		Ball[i] = i + 1;
+	}
+
+	for (int i = 0; i < 6; ++i)
+	{
+		int randNum = rand() % 45;
+		cout << Ball[randNum] << endl;
+	}
+
 	return 0;
 }
